17ee099c809e15b1e889a835fbabcd2985f861fb
John Stassen <john.stassen@gmail.com>
2020-08-29 01:30:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,7 @@ class Transfer
       receiver.balance += amount
       @status = "complete"
     else
-      return "invalid"
+      return "Transaction rejected. Please check your account balance."
     end
   end
 
