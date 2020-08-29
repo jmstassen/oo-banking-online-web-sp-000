26c94a56c5e59a430e46902ce54298fe6b537ac9
John Stassen <john.stassen@gmail.com>
2020-08-29 01:18:52 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,8 @@
 class BankAccount
 
-  attr_reader :name, :balance, :status
-  attr_writer :balance
-  
+  attr_reader :name
+  attr_accessor :balance, :status
+
   def initialize(name)
     @name = name
     @balance = 1000
