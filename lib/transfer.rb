f4cf137e93441ea58e80692cec90c795b907222e
John Stassen <john.stassen@gmail.com>
2020-08-29 01:22:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,10 @@
 class Transfer
+
+  attr_reader :sender, :receiver, :amount
+
   def initialize(sender, receiver, amount)
     @status = "pending"
+    @sender = sender
+    @receiver = receiver
   end
 end
